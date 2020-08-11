c3df55cdcf09416b6603b2df0160597ebb7162a8
Doris <d1882002@gmail.com>
2020-08-11 13:53:44 +0800

    Updates

---
+++ precache-manifest.75f4009bc6951a65d4323670df632c0d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be8f210400abd379aad013f050e653c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8f072143023fd2e5c33",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e142f217439a133bda74",
+    "url": "/static/css/main.016a7b7a.chunk.css"
+  },
+  {
+    "revision": "b8f072143023fd2e5c33",
+    "url": "/static/js/2.27e919a8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.27e919a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e142f217439a133bda74",
+    "url": "/static/js/main.ae8d90ae.chunk.js"
+  },
+  {
+    "revision": "791ac080b92a6024407f",
+    "url": "/static/js/runtime-main.d577d004.js"
+  },
+  {
+    "revision": "090e4629708cbd5860610e834bb10823",
+    "url": "/static/media/fontello.090e4629.ttf"
+  },
+  {
+    "revision": "1f765f74a621b4a792932ac36bca43ed",
+    "url": "/static/media/fontello.1f765f74.woff"
+  },
+  {
+    "revision": "22ecd48201cbe7dac7d58338ae520af0",
+    "url": "/static/media/fontello.22ecd482.woff2"
+  },
+  {
+    "revision": "34d0b7dd8ba0830e0d7f6001dd7177c0",
+    "url": "/static/media/fontello.34d0b7dd.svg"
+  },
+  {
+    "revision": "abcd6752b68218c25e66ed320b003490",
+    "url": "/static/media/fontello.abcd6752.eot"
+  },
+  {
+    "revision": "0df05e7a19e34ad08fd3a1bd3932f4bd",
+    "url": "/static/media/more_vert-black-18dp.0df05e7a.svg"
+  },
+  {
+    "revision": "2e05b272656211c8c7e466e6e2c8d661",
+    "url": "/static/media/pexels-thiago-matos-2335275.2e05b272.jpg"
+  }
+]);
